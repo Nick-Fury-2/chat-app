06798ddcf48a84c3f3e936f6a71411eb703be7b7
Nick-Fury-2 <amanhanda424@gmail.com>
2025-03-21 10:56:47 +0530

    Firebase integration 1.0.0

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 export default function App() {
     return (
         <div>
-            <h1>Hello, world</h1>
+            <h1>Hello, world 2.0</h1>
         </div>
     );
 }
